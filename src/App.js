ffc7632a5d20eac46dd9cdf80d0f114665a2c33b
gsoares <gsoares@wsgc.com>
2022-11-08 13:13:56 -0500

    CRA, tailwindcss v3 and TopNav example

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Topnav from './Components/Topnav/Topnav'
+
+function App() {
+  return (
+    <div className='w-full h-100 bg-slate-300'>
+      <Topnav />
+    </div>
+  )
+}
+
+export default App
